d2fb5f82c33c4ef186915832d12c6ef66a5ed69d
bokboky <H098s02816@chunchon.gwe.hs.kr>
2025-11-11 14:23:41 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
      labels[0]: {
        "texts": ["치킨맛있다"],
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
-    #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
+       "videos": ["https://youtube.com/shorts/zDtGuFHA3CE?si=Fp8ppL7CW-a8cMRV"]
      },
 }
 
